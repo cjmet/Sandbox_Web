68f7bbf1e9236ec6f9f61ba005875c0dbc59086d
CJ Metcalfe <khaaiev@gmail.com>
2024-07-23 17:48:53 -0400

    Added Mobile-ish width media query. Several things not working the way I'd like. Many questions. Added the old questions and code back in for easier reference.

---
+++ PetStore/functions.js
@@ -0,0 +1,4 @@
+function DemoMessage(message, description) { 
+    document.getElementById('MainDataTitleId').innerHTML = message;
+    document.getElementById('MainDataDescriptionId').innerHTML = description;
+}
---
+++ PetStore/script.js
@@ -0,0 +1,2 @@
+console.log('Hello World!');
+DemoMessage('Welcome to Catopia!', 'All Praise Our Feline Overlords! (JavaScript)');
