0f72021e4a1e096c379ce63636bea617b6158f3a
CJ Metcalfe <khaaiev@gmail.com>
2024-07-24 19:27:43 -0400

    ...

---
+++ PetStore/functions.js
@@ -0,0 +1,4 @@
+function demoMessage(message, description) { 
+    document.getElementById('MainDataTitleId').innerHTML = message;
+    document.getElementById('MainDataDescriptionId').innerHTML = description;
+}
---
+++ PetStore/script.js
@@ -0,0 +1,2 @@
+console.log('Hello World!');
+demoMessage('Welcome to Catopia!', 'All Praise Our Feline Overlords! (JavaScript)');
