5acd400aa298175a27bb2935429b6ccde6ce2a4f
cjmet <khaaiev@gmail.com>
2024-08-21 19:50:16 -0400

    Refactored GetWeatherLocation, did more work on the weather API, and added a primitive weather hover/tool-tip.

--- PetStore/script.js
+++ PetStore/script.js
@@ -8,6 +8,7 @@ DemoMessage(
   "All Praise Our Feline Overlords! (JavaScript)"
 );
 
+// Weather Button
 document.getElementById("WeatherButton").addEventListener("click", function () {
   if (document.getElementById("WeatherButton").textContent != "W") {
     document.getElementById("WeatherButton").textContent = "W";
@@ -15,5 +16,70 @@ document.getElementById("WeatherButton").addEventListener("click", function () {
     document.getElementById("WeatherButton").textContent = "w";
   }
 
-  getLocationCallBack(getWeatherAsync);
+  // Weather Button Clicked
+  getWeatherLocationAsync(function (weather) {
+    console.log("[Weather Button] Weather Data");
+    console.log(weather);
+    document.getElementById("WeatherDiv").style.display = "none";
+    document.getElementById("WeatherObservationsDiv").innerHTML =
+      "Observation Station: <br>" +
+      weather.observationStationID +
+      " " +
+      weather.observationStationName +
+      "<br>" +
+      weather.observationShortText +
+      " " +
+      weather.observationTemperature +
+      " " +
+      weather.observationTemperatureUnit +
+      "<br>";
+    document.getElementById("WeatherForecastDiv").innerHTML =
+      "<br>Forecast: <br>" +
+      weather.shortForecast +
+      "<br>" +
+      weather.temperature +
+      " " +
+      weather.temperatureUnit +
+      "<br>" +
+      weather.probabilityOfPrecipitation +
+      "% chance of precipitation<br>" +
+      weather.detailedForecast +
+      "<br>" +
+      weather.forecastStartTime;
+  });
 });
+// /Weather Button
+
+// Weather Widget
+getWeatherLocationAsync(function (weather) {
+  console.log("[Weather Widget] Weather Data");
+  console.log(weather);
+
+  // Short Forecast
+  document.getElementById("WeatherForecast").innerHTML =
+    weather.observationShortText +
+    " " +
+    weather.observationTemperature +
+    weather.observationTemperatureUnit;
+
+  // Weather Image
+
+  // *** Add Code Here  *** //
+
+  // Long Forecast
+  let forecast =
+    weather.shortForecast +
+    " " +
+    weather.temperature +
+    weather.temperatureUnit +
+    "<br>" +
+    weather.probabilityOfPrecipitation +
+    "% precipitation<br>" +
+    '<div id="weatherspacer"><br></div>' +
+    weather.detailedForecast ;
+    // + "<br>" + weather.forecastStartTime;
+  // document.getElementById("WeatherWidget").setAttribute("tooltip", forecast);
+  document.getElementById("WeatherToolTip").innerHTML = forecast;
+});
+
+// /Weather Widget
